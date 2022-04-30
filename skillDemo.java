8acf2a0cf78a867026bc01483397fc304ac3d590
cs15lsp22ahg <cttang@ucsd.edu>
2022-04-30 00:11:24 -0700

    Add Tester

--- skillDemo.java
+++ skillDemo.java
@@ -1,2 +1,8 @@
-public class skillDemo{
+public class skillDemo {
+    public static boolean checkSameStrings(String a, String b) {
+        if (a.equals(b)) {
+            return true;
+        }
+        return false;
+    }
 }
