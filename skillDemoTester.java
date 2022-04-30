6472b6265143021914b282129a998884c95c9c77
cs15lsp22ahg <cttang@ucsd.edu>
2022-04-30 00:13:34 -0700

    Update skillDemoTester.java

---
+++ skillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTester {
+    @Test
+    public void checkSameStringsTest() {
+        assertTrue(skillDemo.checkSameStrings("word", "word"));
+    }
+}
